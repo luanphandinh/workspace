28b84a72e5a0ef558edd95723b7576cb68674682
Luan Phan <luanphandinh@outlook.com>
2021-03-15 19:38:08 +0700

    Add golang pipeline

--- go.sh
+++ go.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 grep -q 'export PATH=$PATH:/usr/local/go/bin' ~/.profile 2>/dev/null || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 grep -q 'export PATH=$PATH:/usr/local/go/bin' ~/.bashrc 2>/dev/null || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+grep -q 'export PATH=$PATH:/usr/local/go/bin' ~/.bashrc 2>/dev/null || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
+GO111MODULE=on go get golang.org/x/tools/gopls@latest
 go version
