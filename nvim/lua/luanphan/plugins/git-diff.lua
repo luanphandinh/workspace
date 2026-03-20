c0a09f570f40de56a7b5c106c980097038d2cc92
dinh.phan <dinh.phan@bytedance.com>
2026-03-20 17:18:47 +0800

    feat: fix git conflict plugin

--- nvim/lua/luanphan/plugins/git-diff.lua
+++ nvim/lua/luanphan/plugins/git-diff.lua
@@ -152,26 +152,16 @@ return function(use)
   -- Git conflict navigation and resolution
   use {
     "akinsho/git-conflict.nvim",
-    version = "*",
-    event = "BufReadPre",
+    tag = "*",
     config = function()
       require("git-conflict").setup({
-        default_mappings = true, -- disable to use custom mappings
-        default_commands = true, -- disable to use custom commands
-        disable_diagnostics = false,
+        default_mappings = true,  -- co: ours, ct: theirs, cb: both, c0: none
+        default_commands = true,
         highlights = {
           incoming = "DiffAdd",
           current = "DiffText",
         },
       })
-
-      -- Custom keymaps for conflict resolution
-      vim.keymap.set("n", "<leader>gco", "<cmd>GitConflictChooseOurs<cr>", { desc = "Choose ours (current)" })
-      vim.keymap.set("n", "<leader>gct", "<cmd>GitConflictChooseTheirs<cr>", { desc = "Choose theirs (incoming)" })
-      vim.keymap.set("n", "<leader>gcb", "<cmd>GitConflictChooseBoth<cr>", { desc = "Choose both" })
-      vim.keymap.set("n", "<leader>gcn", "<cmd>GitConflictNextConflict<cr>", { desc = "Next conflict" })
-      vim.keymap.set("n", "<leader>gcp", "<cmd>GitConflictPrevConflict<cr>", { desc = "Previous conflict" })
-      vim.keymap.set("n", "<leader>gcl", "<cmd>GitConflictListQf<cr>", { desc = "List conflicts in quickfix" })
     end,
   }
 end
