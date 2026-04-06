ee4f3adc244143e10aee059def0d1c089ad8149f
dinh.phan <dinh.phan@bytedance.com>
2026-04-06 12:18:38 +0800

    fix: large file hanging issue

--- nvim/lua/luanphan/plugins/treesitter.lua
+++ nvim/lua/luanphan/plugins/treesitter.lua
@@ -4,10 +4,13 @@ return function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate", -- or `run` if still using older packer
     config = function()
-      -- Enable folding with treesitter
+      -- Tree-sitter folds: use built-in foldexpr (|:help vim.treesitter.foldexpr()|).
+      -- Legacy `nvim_treesitter#foldexpr()` can spin in foldUpdate when switching
+      -- buffers (e.g. nvim_win_set_buf) on large or pathological files.
       vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.opt.foldlevel = 99  -- Start with folds open
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt.foldtext = ""
+      vim.opt.foldlevel = 99
 
       require("nvim-treesitter.configs").setup({
         sync_install = false,
@@ -30,6 +33,20 @@ return function(use)
           "luadoc",
           "markdown",
         },
+      })
+
+      -- Large buffers + TS foldexpr can spin in foldUpdate on buffer switch / win_set_buf.
+      -- Use manual folds so foldexpr is not recomputed.
+      local fold_manual_line_threshold = 10000
+      vim.api.nvim_create_autocmd("BufReadPost", {
+        callback = function(ev)
+          if vim.api.nvim_buf_line_count(ev.buf) > fold_manual_line_threshold then
+            vim.api.nvim_buf_call(ev.buf, function()
+              vim.opt_local.foldmethod = "manual"
+              vim.opt_local.foldexpr = ""
+            end)
+          end
+        end,
       })
     end
   }
